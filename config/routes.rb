fa02d45c98b6cf00dbbf7b4487f563f5f1b403c9
brenothales <breno.tahles@gmail.com>
2018-05-23 22:23:55 -0300

    frist commit

---
+++ config/routes.rb
@@ -0,0 +1,15 @@
+Rails.application.routes.draw do
+  get 'home/index'
+
+  get 'home/mvc'
+
+  get 'home/http'
+
+  get 'home/frameworks'
+
+  get 'home/api'
+
+  get 'home/git'
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
